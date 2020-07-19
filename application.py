533080b6887ee69a4e1d993d33b3959e80ef741e
JadeBlue961 <dragonflareful@gmail.com>
2020-07-19 19:58:30 +0300

    Deployment test.

--- application.py
+++ application.py
@@ -4,11 +4,12 @@ from os import environ
 
 from src import create_app
 
+'''
 HOST = environ.get('SERVER_HOST', 'localhost')
 try:
     PORT = int(environ.get('SERVER_PORT', '5000'))
 except ValueError:
     PORT = 5000
-
-app = create_app(os.getenv('FLASK_ENV') or 'default')
-app.run() #run app in debug mode on port
+'''
+application = create_app(os.getenv('FLASK_ENV') or 'default')
+#application.run() #run app in debug mode on port
